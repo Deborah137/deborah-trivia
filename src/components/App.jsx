32187d2fa7703e517de389e01417973086157dc6
Deborah kwong <42001974+Deborah137@users.noreply.github.com>
2021-01-21 17:00:46 -0500

    finish day one basics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component, useState } from "react";
+import { isPropertySignature } from "typescript";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+export function Question(props) {
+  return <div> {props.question}!</div>;
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  return (
+    <div className="app">
+      Trivia!
+      <Question question={data[0].question.text} />
+      <Question question="question two goes here" />
+    </div>
+  );
+  // is this okay
+  let questionumber = 0;
 }
 
 export default App;
