4c12fb662769c7a7cb8776c094702a685202cd61
Deborah kwong <42001974+Deborah137@users.noreply.github.com>
2021-02-01 17:48:00 -0500

    finish rendering button and partial on day two

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,41 @@ import { isPropertySignature } from "typescript";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+//- [ ] Pass props for the answer choices into `<Question />`.
+
 export function Question(props) {
-  return <div> {props.question}!</div>;
+  return (
+    <div>
+      <div>{props.question}</div>
+      <Answer answer={props.answer} />
+    </div>
+  );
+}
+
+export function NextQuestion(props) {
+  return <button>Next Question</button>;
+}
+
+export function Answer(props) {
+  return <div> {props.answer} </div>;
 }
 // is this okay
+
 let questnum = 0;
 
 function App() {
   return (
     <div className="app">
       Trivia!
-      <Question question={data[questnum].question.text} />
-      <Question question="question two goes here" />
+      <Question question={data[questnum].question.text} answer="my answer" />
+      <Question question="question two goes here" answer="my other answer" />
+      <NextQuestion />
     </div>
   );
 }
 
 export default App;
+
+// These are equivalent:
+// <button></button>
+// <button />
