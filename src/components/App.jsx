22d9cd7d628b0ef602c3f3394aa7d70e69496a5d
Deborah kwong <42001974+Deborah137@users.noreply.github.com>
2021-03-22 17:16:23 -0400

    day 3 goal 1 finished it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component, useState } from "react";
-import { isPropertySignature } from "typescript";
 import "../css/App.css";
 import data from "../sample_data.json";
 
@@ -22,7 +21,7 @@ export function Question(props) {
 }
 
 export function NextQuestion(props) {
-  return <button>Next Question</button>;
+  return <button onClick={props.onClick}>Next Question</button>;
 }
 
 export function CorrectAnswer(props) {
@@ -47,18 +46,21 @@ function App() {
   let [questnum, setQuestNum] = useState(0);
 
   let [isAnswered, setIsAnswered] = useState(false);
-
+  const correctAnswer = data[questnum].question.choices[
+    data[questnum].question.correct_choice_index
+  ];  
+  
   return (
     <div className="app">
       Trivia!
       <Question
         question={data[questnum].question.text}
-        answer={["ChoiceA", "ChoiceB", "ChoiceC", "ChoiceD"]}
+        answer={data[questnum].question.choices}
       />
       {/* <Question question="question two goes here" answer=["my other answer"] /> */}
-      <NextQuestion />
+      <NextQuestion onClick={() => setQuestNum(questnum + 1)} />
       <CorrectAnswer onClick={() => setIsAnswered(!isAnswered)} />
-      {isAnswered === true ? <p>This question has been answered.</p> : <p></p>}
+      {isAnswered === true ? <p>The correct answer is {correctAnswer}</p> : <p></p>}
     </div>
   );
 }
