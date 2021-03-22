90d249db691b890c7f041f43ca864cf65dc38bef
Deborah kwong <42001974+Deborah137@users.noreply.github.com>
2021-03-22 17:52:16 -0400

    update css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,16 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
-//- [ ] Pass props for the answer choices into `<Question />`.
-// let pieFlavor = ["blueberry", "pumpkin", "chocolate"];
-
-// let dessert = pieFlavor.map((flavor) => {
-// 	return flavor + " pie with whipped cream";
-// });
-
 export function Question(props) {
   return (
     <div>
       <div>{props.question}</div>
-      {props.answer.map((answer) => {
-        return <Answer answer={answer} />;
-      })}
+
+      <div class="answers">
+        {props.answer.map((answer) => {
+          return <Answer answer={answer} />;
+        })}
+      </div>
     </div>
   );
 }
@@ -31,47 +27,32 @@ export function CorrectAnswer(props) {
 export function Answer(props) {
   return <div> {props.answer} </div>;
 }
-// is this okay
-
-// declaring a regular variable
-
-// return <button onClick={
-//   () => setIsAnswered (true)
-// }>
 
 function App() {
-  // declaring a React state
-  // let [ stateVariableName, setUpdateStateFunction ]
-  //  = useState(initialState)
   let [questnum, setQuestNum] = useState(0);
 
   let [isAnswered, setIsAnswered] = useState(false);
-  const correctAnswer = data[questnum].question.choices[
-    data[questnum].question.correct_choice_index
-  ];  
-  
+  const correctAnswer =
+    data[questnum].question.choices[
+      data[questnum].question.correct_choice_index
+    ];
+
   return (
     <div className="app">
-      Trivia!
+      <div class="triva">Trivia!</div>
       <Question
         question={data[questnum].question.text}
         answer={data[questnum].question.choices}
       />
-      {/* <Question question="question two goes here" answer=["my other answer"] /> */}
       <NextQuestion onClick={() => setQuestNum(questnum + 1)} />
       <CorrectAnswer onClick={() => setIsAnswered(!isAnswered)} />
-      {isAnswered === true ? <p>The correct answer is {correctAnswer}</p> : <p></p>}
+      {isAnswered === true ? (
+        <p>The correct answer is {correctAnswer}</p>
+      ) : (
+        <p></p>
+      )}
     </div>
   );
 }
 
-//{ if(isAnswered === True){
-// setIsAnswered("<p>This question has been answered.</p>")} }
-
-// setisanswered
-
 export default App;
-
-// These are equivalent:
-// <button></button>
-// <button />
